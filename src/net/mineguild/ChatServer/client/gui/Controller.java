182725eb13ae3faad3db872f306537ee321d9fe4
Korbinian Stein <korbinian.stein@googlemail.com>
2015-12-02 22:20:53 +0100

    Little updates.

--- src/net/mineguild/ChatServer/client/gui/Controller.java
+++ src/net/mineguild/ChatServer/client/gui/Controller.java
@@ -1,29 +1,49 @@
 package net.mineguild.ChatServer.client.gui;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
-public class Controller {
+import java.net.URL;
+import java.util.ResourceBundle;
 
-    @FXML private Parent rootPane;
-    @FXML private TextArea messageArea;
-    @FXML private TextField inputField;
+public class Controller implements Initializable {
+
+    @FXML
+    private Parent rootPane;
+    @FXML
+    private TextArea messageArea;
+    @FXML
+    private TextField inputField;
 
     private App client;
 
-    public Controller(App client){
+    public Controller(App client) {
         this.client = client;
     }
 
-    public Controller(){
+    public Controller() {
     }
 
-    public void handleSend(ActionEvent event){
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        messageArea.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String s, String s2) {
+
+            }
+        });
+    }
+
+
+    public void handleSend(ActionEvent event) {
         client.sendMessage(inputField.getText());
         inputField.clear();
     }
@@ -38,10 +58,9 @@ public class Controller {
         );
     }
 
-    public TextArea getTextArea(){
+    public TextArea getTextArea() {
         System.out.println(messageArea);
         return messageArea;
     }
-
 
 }
